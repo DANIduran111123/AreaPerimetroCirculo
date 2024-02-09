64ccde150d6cca02b6153d27abb20a419faed56b
DANIduran111123 <danieladuran@colegioguanenta.edu.co>
2024-02-09 08:22:04 -0500

    Se agregó codigo python del programa

---
+++ AreaPerimetroCirculo.py
@@ -0,0 +1,15 @@
+#programa para calcular el area y el perimetro  de un circulo de radio R 
+
+import  math 
+
+#input 
+r= input("ingrese el radio: ")
+r=int(r)
+
+#processing
+a=math .pi* r**2
+p= 2 * math.pi *r
+
+# output 
+print ("El area es: " + str (a))
+print ("El perimetro es: "+ str (p))
